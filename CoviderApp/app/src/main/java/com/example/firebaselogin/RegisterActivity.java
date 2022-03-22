92ee3a6cf397047214ba09d7e4535b111a7ee10b
zhangeth <zhangeth@usc.edu>
2022-03-21 21:59:21 -0700

    adding all user fields: email, name, username, password

--- CoviderApp/app/src/main/java/com/example/firebaselogin/RegisterActivity.java
+++ CoviderApp/app/src/main/java/com/example/firebaselogin/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,13 +13,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+    public static final String EMAIL_KEY = "email";
+    public static final String NAME_KEY = "name";
+    public static final String USERNAME_KEY = "username";
+    public static final String PASSWORD_KEY = "password";
+    public static final String TAG = "User";
+    //document instance to save to firestore
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("data/users");
     private FirebaseAuth mAuth;
-    private EditText email, password;
+    private EditText email, name, username, password;
     private Button btnRegister;
     private TextView textLogin;
     @Override
@@ -27,7 +42,11 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
         email = findViewById(R.id.register_email);
+
         password = findViewById(R.id.register_password);
+        name = findViewById(R.id.register_name);
+        username = findViewById(R.id.register_username);
+
         btnRegister  = findViewById(R.id.register);
         textLogin = findViewById(R.id.text_login);
 
@@ -49,19 +68,23 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void Register()
     {
-        String user = email.getText().toString().trim();
-        String pass = password.getText().toString().trim();
-        if(user.isEmpty())
-        {
+        String emailTxt = email.getText().toString().trim();
+        String nameTxt = name.getText().toString().trim();
+        String usernameTxt = username.getText().toString().trim();
+        String passTxt = password.getText().toString().trim();
+
+        if(emailTxt.isEmpty())
             email.setError("Email can not be empty");
-        }
-        if(pass.isEmpty())
-        {
+        if(nameTxt.isEmpty())
+            name.setError("Name can not be empty");
+        if(usernameTxt.isEmpty())
+            username.setError("Username can not be empty");
+        if(passTxt.isEmpty())
             password.setError("Password can not be empty");
-        }
+
         else
         {
-            mAuth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(emailTxt, passTxt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful())
@@ -76,6 +99,25 @@ public class RegisterActivity extends AppCompatActivity {
 
                 }
             });
+            //add username, password, name to firestore user doc
+            Map<String, Object> dataToSave = new HashMap<String, Object>();
+            dataToSave.put(EMAIL_KEY, emailTxt);
+            dataToSave.put(NAME_KEY, nameTxt);
+            dataToSave.put(USERNAME_KEY, usernameTxt);
+            dataToSave.put(PASSWORD_KEY, passTxt);
+
+            mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Log.d(TAG, "Document has been saved!");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w(TAG, "Document was not saved", e);
+                }
+            });
+            
         }
     }
 }

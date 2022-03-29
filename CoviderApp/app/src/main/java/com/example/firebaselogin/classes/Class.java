8a1a25e6cfdbbf84924112f00587bf2b7f8b7ce0
zhangeth <zhangeth@usc.edu>
2022-03-28 17:44:39 -0700

    calendar/classes added

---
+++ CoviderApp/app/src/main/java/com/example/firebaselogin/classes/Class.java
@@ -0,0 +1,80 @@
+package com.example.firebaselogin.classes;
+
+import com.example.firebaselogin.enums.InstructStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Class {
+    private int ID;
+    private String name;
+    public Date date;
+    private Instructor instructor;
+    private List<Student> students;
+    private Building building;
+    private InstructStatus instructStatus;
+
+    //constructors
+    public Class(String name, Instructor instructor, Building building, InstructStatus instructStatus){
+        this.name = name;
+        this.instructor = instructor;
+        this.building = building;
+        this.instructStatus = instructStatus;
+
+        students = new ArrayList<>();
+    }
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public Building getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Building building) {
+        this.building = building;
+    }
+
+    public InstructStatus getInstructStatus() {
+        return instructStatus;
+    }
+
+    public void setInstructStatus(InstructStatus instructStatus) {
+        this.instructStatus = instructStatus;
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+
+}
---
+++ CoviderApp/app/src/main/java/com/example/firebaselogin/classes/Schedule.java
@@ -0,0 +1,9 @@
+package com.example.firebaselogin.classes;
+
+import java.util.Date;
+import java.util.Map;
+
+public class Schedule {
+    private Map<Date, Class> schedule;
+
+}
